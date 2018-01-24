1b563ae0d939a1876fca619b7013093374b76b5b
Justin Le <justin@jle.im>
2018-01-24 15:27:57 -0800

    a pure instance for MonadSample

--- src/Numeric/Opto/Sample.hs
+++ src/Numeric/Opto/Sample.hs
@@ -5,12 +5,14 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase                 #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE TupleSections              #-}
 {-# LANGUAGE UnboxedTuples              #-}
 {-# LANGUAGE UndecidableInstances       #-}
 
 module Numeric.Opto.Sample (
     MonadSample(..)
-  , SampleRefT, runSampleRefT, sampleRefT
+  , SampleRefT, runSampleRefT, foldSampleRefT, sampleRefT
+  , SampleFoldT, foldSampleFoldT, sampleFoldT
   ) where
 
 import           Control.Applicative
@@ -18,6 +20,9 @@ import           Control.Monad
 import           Control.Monad.Primitive
 import           Control.Monad.Trans.Maybe
 import           Control.Monad.Trans.Reader
+import           Control.Monad.Trans.State
+import           Data.Foldable
+import           Data.Profunctor
 import           Numeric.Opto.Ref
 
 class MonadPlus m => MonadSample r m | m -> r where
@@ -35,6 +40,12 @@ newtype SampleRefT v r m a = SampleRefT { sampleRefReader :: ReaderT v (MaybeT m
 runSampleRefT :: SampleRefT v r m a -> v -> m (Maybe a)
 runSampleRefT = fmap runMaybeT . runReaderT . sampleRefReader
 
+foldSampleRefT :: (Ref m [r] v, Foldable t) => SampleRefT v r m a -> t r -> m (Maybe a, [r])
+foldSampleRefT sr xs = do
+    r <- newRef (toList xs)
+    y <- runSampleRefT sr r
+    (y,) <$> readRef r
+
 sampleRefT :: (v -> m (Maybe a)) -> SampleRefT v r m a
 sampleRefT = SampleRefT . ReaderT . fmap MaybeT
 
@@ -43,4 +54,19 @@ instance (Monad m, Ref m [r] v) => MonadSample r (SampleRefT v r m) where
       updateRef' v $  \case
         []   -> ([], Nothing)
         x:xs -> (xs, Just x )
+
+newtype SampleFoldT r m a = SampleFoldT { sampleFoldState :: MaybeT (StateT [r] m) a }
+    deriving ( Functor
+             , Applicative
+             , Monad
+             , PrimMonad
+             , Alternative
+             , MonadPlus
+             )
+
+foldSampleFoldT :: Foldable t => SampleFoldT r m a -> t r -> m (Maybe a, [r])
+foldSampleFoldT = lmap toList . runStateT . runMaybeT . sampleFoldState
+
+sampleFoldT :: ([r] -> m (Maybe a, [r])) -> SampleFoldT r m a
+sampleFoldT = SampleFoldT . MaybeT . StateT
 
