0deafab12f5b181adedc451ec957da5f792f289e
Justin Le <justin@jle.im>
2018-01-23 23:34:58 -0800

    added everything back in

--- src/Numeric/Opto.hs
+++ src/Numeric/Opto.hs
@@ -15,22 +15,20 @@
 {-# LANGUAGE TypeSynonymInstances   #-}
 {-# LANGUAGE UndecidableInstances   #-}
 
-module Numeric.Opto where
+module Numeric.Opto (
+    Ref(..)
+  , Additive(..), Scaling(..)
+  , AdditiveInPlace(..), ScalingInPlace(..)
+  , OptoM(..), Opto
+  , fromCopying, fromPure
+  , scanOptoM, scanOpto
+  , scanOptoUntilM, scanOptoUntil
+  , iterateOptoM, iterateOpto
+  , sgdOptimizerM, sgdOptimizer
+  , gdOptimizerM, gdOptimizer
+  , Adam(..), adamOptimizerM, adamOptimizer
+  ) where
 
--- module Numeric.Opto (
---     Ref(..)
---   , OptoM(..), Opto
---   , fromCopying, fromPure
---   , scanOptoM, scanOpto
---   , scanOptoUntilM, scanOptoUntil
---   , iterateOptoM, iterateOpto
---   , sgdOptimizerM, sgdOptimizer
---   , gdOptimizerM, gdOptimizer
---   , Adam(..), adamOptimizerM, adamOptimizer
---   ) where
-
--- import           Data.Foldable
--- import           Data.Primitive
 import           Control.Monad
 import           Control.Monad.Primitive
 import           Control.Monad.ST
@@ -39,11 +37,9 @@ import           Control.Monad.Trans.Maybe
 import           Data.Default
 import           Data.Kind
 import           Data.Primitive.MutVar
-import           Data.Proxy
 import           Unsafe.Coerce
 import qualified Data.Vector               as V
 import qualified Data.Vector.Mutable       as MV
-import qualified Data.Vector.Sized         as SV
 
 class Ref m a v | v -> a where
     newRef     :: a -> m v
@@ -53,6 +49,7 @@ class Ref m a v | v -> a where
     modifyRef' :: v -> (a -> a) -> m ()
 
 class Additive a where
+    infixl 6 .+.
     (.+.)   :: a -> a -> a
     addZero :: a
 
@@ -62,6 +59,7 @@ class Additive a where
     addZero = 0
 
 class (Num c, Additive a) => Scaling c a | a -> c where
+    infixl 7 .*
     (.*)     :: c -> a -> a
     scaleOne :: c
 
@@ -70,13 +68,31 @@ class (Num c, Additive a) => Scaling c a | a -> c where
     default scaleOne :: Num c => c
     scaleOne = 1
 
+-- class Additive a => Multiplicative a where
+--     infixl 7 .*.
+--     (.*.)     :: a -> a -> a
+--     infixl 7 ./.
+--     (./.)     :: a -> a -> a
+--     multRecip :: a -> a
+--     multOne   :: a
+--     default (.*.) :: Num a => a -> a -> a
+--     (.*.) = (*)
+--     default (./.) :: Fractional a => a -> a -> a
+--     (./.) = (/)
+--     multRecip = (multOne ./.)
+--     default multOne :: Num a => a
+--     multOne = 1
+
 class (Ref m a v, Additive a) => AdditiveInPlace m v a where
+    infix 4 .+.=
     (.+.=) :: v -> a -> m ()
     r .+.= x = modifyRef' r (.+. x)
 
 class (AdditiveInPlace m v a, Scaling c a) => ScalingInPlace m v c a where
+    infix 4 .*=
     (.*=)  :: v -> c -> m ()
     r .*= c = modifyRef' r (c .*)
+    infix 4 .*+=
     (.*+=) :: v -> (c, a) -> m ()
     r .*+= (c, x) = modifyRef' r ((c .* x) .+.)
 
@@ -108,6 +124,7 @@ instance Applicative m => Ref m EmptyRef EmptyRef where
 
 instance Additive Double
 instance Scaling Double Double
+-- instance Multiplicative Double
 instance Ref m Double v => AdditiveInPlace m v Double
 instance Ref m Double v => ScalingInPlace m v Double Double
 
@@ -118,6 +135,12 @@ instance Num a => Additive (V.Vector a) where
 instance Num a => Scaling a (V.Vector a) where
     c .* xs  = (c *) <$> xs
     scaleOne = 1
+
+-- instance Fractional a => Multiplicative (V.Vector a) where
+--     (.*.) = V.zipWith (*)
+--     (./.) = V.zipWith (/)
+--     multRecip = fmap recip
+--     multOne = undefined
 
 instance (PrimMonad m, PrimState m ~ s, Num a) => AdditiveInPlace m (MV.MVector s a) (V.Vector a) where
     r .+.= xs = flip V.imapM_ xs $ \i x ->
@@ -136,10 +159,30 @@ data OptoM :: (Type -> Type) -> Type -> Type -> Type -> Type where
 
 type Opto s = OptoM (ST s)
 
+fromCopying
+    :: (PrimMonad m, ScalingInPlace m v c a)
+    => s
+    -> (r -> a -> s -> m (c, a, s))
+    -> OptoM m v r a
+fromCopying s0 update =
+    MkOptoM { oInit   = s0
+            , oUpdate = \rS r x -> do
+                (c, g, s) <- update r x =<< readMutVar rS
+                writeMutVar rS s
+                return (c, g)
+            }
+
+fromPure
+    :: (PrimMonad m, ScalingInPlace m v c a)
+    => s
+    -> (r -> a -> s -> (c, a, s))
+    -> OptoM m v r a
+fromPure s0 update = fromCopying s0 (\r x -> pure . update r x)
+
 scanOptoUntilM
     :: forall m v r a t. (Monad m, Foldable t)
     => t r
-    -> (a -> a -> m Bool)       -- ^ grad, current
+    -> (a -> a -> m Bool)       -- ^ step, current
     -> a
     -> OptoM m v r a
     -> m (a, OptoM m v r a)
@@ -147,15 +190,57 @@ scanOptoUntilM rs stop x0 MkOptoM{..} = do
     rS <- newRef oInit
     rX <- newRef @m @a @v x0
     _ <- runMaybeT . forM_ rs $ \r -> do
-      (y, g) <- lift $ do
+      (y, step) <- lift $ do
         y      <- readRef rX
         (c, g) <- oUpdate rS r =<< readRef rX
         rX .*+= (c, g)
-        return (y, g)
-      guard . not =<< lift (stop g y)
+        return (y, c .* g)
+      guard . not =<< lift (stop step y)
     s' <- readRef rS
     let o' = MkOptoM s' oUpdate
     (, o') <$> readRef rX
+
+scanOptoUntil
+    :: Foldable t
+    => t r
+    -> (a -> a -> Bool)         -- ^ step, current
+    -> a
+    -> (forall s'. Opto s' v r a)
+    -> (a, Opto s v r a)
+scanOptoUntil xs stop y0 o0 = runST $ do
+    (y', o') <- scanOptoUntilM xs (\s -> pure . stop s) y0 o0
+    return (y', unsafeCoerce o')        -- is this safe?  probably.
+
+scanOptoM
+    :: (Monad m, Foldable t)
+    => t r
+    -> a
+    -> OptoM m v r a
+    -> m (a, OptoM m v r a)
+scanOptoM xs = scanOptoUntilM xs (\_ _ -> pure False)
+
+scanOpto
+    :: Foldable t
+    => t r
+    -> a
+    -> (forall s'. Opto s' v r a)
+    -> (a, Opto s v r a)
+scanOpto xs = scanOptoUntil xs (\_ _ -> False)
+
+iterateOptoM
+    :: Monad m
+    => (a -> a -> m Bool)   -- ^ step, current
+    -> a
+    -> OptoM m v () a
+    -> m (a, OptoM m v () a)
+iterateOptoM = scanOptoUntilM (repeat ())
+
+iterateOpto
+    :: (a -> a -> Bool)   -- ^ step, current
+    -> a
+    -> (forall s'. Opto s' v () a)
+    -> (a, Opto s v () a)
+iterateOpto = scanOptoUntil (repeat ())
 
 sgdOptimizerM
     :: (Ref m a v, ScalingInPlace m v c a, Applicative m)
@@ -174,187 +259,58 @@ sgdOptimizer
     -> OptoM m v r a
 sgdOptimizer lr gr = sgdOptimizerM lr (\x -> pure . gr x)
 
--- fromCopying
---     :: PrimMonad m
---     => s
---     -> (a -> b -> m b)
---     -> (b -> b -> s -> m (b, s))  -- ^ gradient, value, state
---     -> OptoM m a b
--- fromCopying s0 grad update =
---     MkOptoM { oSVar   = Proxy
---             , oInit   = s0
---             , oGrad   = grad
---             , oUpdate = \rS rY g -> do
---                 y <- readMutVar rY
---                 s <- readMutVar rS
---                 (y', s') <- update g y s
---                 writeMutVar rS s'
---                 writeMutVar rY y'
---             }
+gdOptimizerM
+    :: (ScalingInPlace m v c a, Applicative m)
+    => c
+    -> (a -> m a)           -- ^ gradient
+    -> OptoM m v () a
+gdOptimizerM lr upd = sgdOptimizerM lr (const upd)
 
--- fromPure
---     :: PrimMonad m
---     => s
---     -> (a -> b -> b)
---     -> (b -> b -> s -> (b, s))  -- ^ gradient, value, state
---     -> OptoM m a b
--- fromPure s0 grad update = fromCopying s0 (\x   -> return . grad x)
---                                          (\g y -> return . update g y)
+gdOptimizer
+    :: (ScalingInPlace m v c a, Applicative m)
+    => c
+    -> (a -> a)             -- ^ gradient
+    -> OptoM m v () a
+gdOptimizer r gr = sgdOptimizer r (const gr)
 
--- scanOptoM
---     :: (Monad m, Foldable t)
---     => t a
---     -> b
---     -> OptoM m a b
---     -> m (b, OptoM m a b)
--- scanOptoM xs = scanOptoUntilM xs (\_ _ -> pure False)
+data Adam c = Adam
+    { adamStep    :: !c
+    , adamDecay1  :: !c
+    , adamDecay2  :: !c
+    , adamEpsilon :: !c
+    }
+  deriving (Show, Eq)
 
--- scanOpto
---     :: Foldable t
---     => t a
---     -> b
---     -> (forall s'. Opto s' a b)
---     -> (b, Opto s a b)
--- scanOpto xs = scanOptoUntil xs (\_ _ -> False)
+instance Fractional a => Default (Adam a) where
+    def = Adam { adamStep    = 0.001
+               , adamDecay1  = 0.9
+               , adamDecay2  = 0.999
+               , adamEpsilon = 1e-8
+               }
 
--- scanOptoUntilM
---     :: (Monad m, Foldable t)
---     => t a
---     -> (b -> b -> m Bool)       -- ^ grad, current
---     -> b
---     -> OptoM m a b
---     -> m (b, OptoM m a b)
--- scanOptoUntilM xs stop y0 MkOptoM{..} = do
---     rS <- newRef oInit
---     rY <- newRef y0
---     _ <- runMaybeT . forM_ xs $ \x -> do
---       y <- lift $ readRef rY
---       g <- lift $ oGrad x =<< readRef rY
---       guard . not =<< lift (stop g y)
---       lift $ oUpdate rS rY g
---     s' <- readRef rS
---     let o' = MkOptoM Proxy s' oGrad oUpdate
---     (, o') <$> readRef rY
+adamOptimizerM
+    :: forall m v r a c. (RealFloat c, Floating a, ScalingInPlace m v c a, PrimMonad m)
+    => Adam c
+    -> (r -> a -> m a)          -- ^ gradient
+    -> OptoM m v r a
+adamOptimizerM Adam{..} gr = 
+    MkOptoM { oInit   = (scaleOne @_ @a, addZero, addZero)
+            , oUpdate = \rS r x -> do
+                g <- gr r x
+                (t, m0, v0) <- readMutVar rS
+                let m1 = adamDecay1 .* m0 .+. (1 - adamDecay1) .* g
+                    v1 = adamDecay2 .* v0 .+. (1 - adamDecay2) .* g
+                    mHat = recip (1 - adamDecay1 ** t) .* m1
+                    vHat = recip (1 - adamDecay2 ** t) .* v1
+                writeMutVar rS (t + 1, m1, v1)
+                return ( -adamStep
+                       , mHat / (sqrt vHat + realToFrac adamEpsilon)
+                       )
+            }
 
--- scanOptoUntil
---     :: Foldable t
---     => t a
---     -> (b -> b -> Bool)         -- ^ grad, current
---     -> b
---     -> (forall s'. Opto s' a b)
---     -> (b, Opto s a b)
--- scanOptoUntil xs stop y0 o0 = runST $ do
---     (y', o') <- scanOptoUntilM xs (\g -> pure . stop g) y0 o0
---     return (y', unsafeCoerce o')        -- is this safe?  probably.
-
--- iterateOptoM
---     :: Monad m
---     => (b -> b -> m Bool)   -- ^ grad, current
---     -> b
---     -> OptoM m () b
---     -> m (b, OptoM m () b)
--- iterateOptoM = scanOptoUntilM (repeat ())
-
--- iterateOpto
---     :: (b -> b -> Bool)   -- ^ grad, current
---     -> b
---     -> (forall s'. Opto s' () b)
---     -> (b, Opto s () b)
--- iterateOpto = scanOptoUntil (repeat ())
-
--- sgdOptimizerM
---     :: ()
---     => Double
---     -> (a -> b -> m b)          -- ^ gradient
---     -> (v -> b -> m ())         -- ^ adding action
---     -> OptoM m a b
--- sgdOptimizerM r gr upd =
---     MkOptoM { oSVar   = Proxy @()
---             , oInit   = ()
---             , oGrad   = gr
---             , oUpdate = \_ rY g -> upd rY (realToFrac (- r) * g)
---             }
-
-
--- sgdOptimizerM
---     :: (Fractional b, Ref m b v, Applicative m)
---     => Double
---     -> (a -> b -> m b)          -- ^ gradient
---     -> (v -> b -> m ())         -- ^ adding action
---     -> OptoM m a b
--- sgdOptimizerM r gr upd =
---     MkOptoM { oSVar   = Proxy @()
---             , oInit   = ()
---             , oGrad   = gr
---             , oUpdate = \_ rY g -> upd rY (realToFrac (- r) * g)
---             }
-
--- sgdOptimizer
---     :: (Fractional b, PrimMonad m)
---     => Double
---     -> (a -> b -> b)
---     -> OptoM m a b
--- sgdOptimizer r gr = sgdOptimizerM r (\x -> return . gr x) $ \rY u ->
---     modifyMutVar' rY (+ u)
-
--- gdOptimizerM
---     :: (Fractional b, Ref m b v, Applicative m)
---     => Double
---     -> (b -> b)             -- ^ gradient
---     -> (v -> b -> m ())     -- ^ adding action
---     -> OptoM m () b
--- gdOptimizerM r gr upd = sgdOptimizerM r (const (pure . gr)) upd
-
--- gdOptimizer
---     :: (Fractional b, PrimMonad m)
---     => Double
---     -> (b -> b)             -- ^ gradient
---     -> OptoM m () b
--- gdOptimizer r gr = sgdOptimizer r (const gr)
-
--- data Adam = Adam
---     { adamStep    :: Double
---     , adamDecay1  :: Double
---     , adamDecay2  :: Double
---     , adamEpsilon :: Double
---     }
---   deriving (Show, Eq)
-
--- instance Default Adam where
---     def = Adam { adamStep    = 0.001
---                , adamDecay1  = 0.9
---                , adamDecay2  = 0.999
---                , adamEpsilon = 1e-8
---                }
-
--- adamOptimizerM
---     :: forall a b v m. (Floating b, Ref m b v, PrimMonad m)
---     => Adam
---     -> (a -> b -> m b)          -- ^ gradient
---     -> (v -> b -> m ())         -- ^ adding
---     -> OptoM m a b
--- adamOptimizerM Adam{..} gr upd = 
---     MkOptoM { oSVar   = Proxy @(MutVar (PrimState m) (Double, b, b))
---             , oInit   = (1,0,0)
---             , oGrad   = gr
---             , oUpdate = \rS rB g -> do
---                 (t, m0, v0) <- readRef rS
---                 let m1 = realToFrac adamDecay1 * m0
---                        + realToFrac (1 - adamDecay1) * g
---                     v1 = realToFrac adamDecay2 * v0
---                        + realToFrac (1 - adamDecay2) * g
---                     mHat = m1 / (1 - realToFrac (adamDecay1 ** t))
---                     vHat = v1 / (1 - realToFrac (adamDecay2 ** t))
---                 writeRef rS (t + 1, m1, v1)
---                 upd rB $ realToFrac (- adamStep)
---                          * mHat
---                          / (sqrt vHat + realToFrac adamEpsilon)
---             }
-
--- adamOptimizer
---     :: (Floating b, PrimMonad m)
---     => Adam
---     -> (a -> b -> b)          -- ^ gradient
---     -> OptoM m a b
--- adamOptimizer a gr = adamOptimizerM a (\x -> return . gr x) $ \rY u ->
---     modifyMutVar' rY (+ u)
+adamOptimizer
+    :: (RealFloat c, Floating a, ScalingInPlace m v c a, PrimMonad m)
+    => Adam c
+    -> (r -> a -> a)          -- ^ gradient
+    -> OptoM m v r a
+adamOptimizer a gr = adamOptimizerM a (\x -> pure . gr x)
