6b2e35c1f05585edfefb64a6e78d53030767433e
Justin Le <justin@jle.im>
2018-01-28 18:17:13 -0800

    batching sampler

--- src/Control/Monad/Sample.hs
+++ src/Control/Monad/Sample.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE DeriveFunctor              #-}
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE FunctionalDependencies     #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE KindSignatures             #-}
 {-# LANGUAGE LambdaCase                 #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE TupleSections              #-}
+{-# LANGUAGE TypeApplications           #-}
 {-# LANGUAGE UnboxedTuples              #-}
 {-# LANGUAGE UndecidableInstances       #-}
 
@@ -13,6 +17,8 @@ module Control.Monad.Sample (
     MonadSample(..)
   , SampleRefT, runSampleRefT, foldSampleRefT, sampleRefT
   , SampleFoldT, foldSampleFoldT, sampleFoldT
+  , SampleFold, foldSampleFold, sampleFold
+  , Batching(..)
   ) where
 
 import           Control.Applicative
@@ -23,7 +29,11 @@ import           Control.Monad.Trans.Reader
 import           Control.Monad.Trans.State
 import           Data.Bifunctor
 import           Data.Foldable
+import           Data.Functor.Identity
+import           Data.List.Split
 import           Data.Profunctor
+import           Data.Proxy
+import           GHC.TypeLits
 import           Numeric.Opto.Ref
 
 class MonadPlus m => MonadSample r m | m -> r where
@@ -74,9 +84,31 @@ foldSampleFoldT = lmap toList . runStateT . runMaybeT . sampleFoldState
 sampleFoldT :: ([r] -> m (Maybe a, [r])) -> SampleFoldT r m a
 sampleFoldT = SampleFoldT . MaybeT . StateT
 
+type SampleFold r = SampleFoldT r Identity
+
+foldSampleFold :: Foldable t => SampleFold r a -> t r -> (Maybe a, [r])
+foldSampleFold sf = runIdentity . foldSampleFoldT sf
+
+sampleFold :: Monad m => ([r] -> (Maybe a, [r])) -> SampleFoldT r m a
+sampleFold = SampleFoldT . MaybeT . StateT . fmap return
+
 instance Monad m =>  MonadSample r (SampleFoldT r m) where
-    sample = sampleFoldT . fmap return $ \case []   -> (Nothing, [])
-                                               x:xs -> (Just x , xs)
-    sampleN n = sampleFoldT . fmap return $
+    sample = sampleFold $ \case []   -> (Nothing, [])
+                                x:xs -> (Just x , xs)
+    sampleN n = sampleFold $
       first (mfilter (not . null) . Just) . splitAt n
 
+newtype Batching (n :: Nat) m a = Batching { runBatching :: m a }
+    deriving ( Functor
+             , Applicative
+             , Monad
+             , PrimMonad
+             , Alternative
+             , MonadPlus
+             )
+
+instance (KnownNat n, MonadSample r m) => MonadSample [r] (Batching n m) where
+    sample    = Batching $ sampleN (fromIntegral (natVal (Proxy @n)))
+    sampleN m = Batching $ chunksOf n <$> sampleN (n * m)
+      where
+        n = fromIntegral $ natVal (Proxy @n)
