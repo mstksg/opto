bae38e6147d4b747582c145650e44363e3182c7c
Justin Le <justin@jle.im>
2019-07-10 16:29:53 -0700

    runner now takes options

--- app/opto-neural.hs
+++ app/opto-neural.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE GADTs                 #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RecordWildCards       #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TupleSections         #-}
@@ -14,10 +15,11 @@
 {-# LANGUAGE ViewPatterns          #-}
 {-# OPTIONS_GHC -fno-warn-orphans  #-}
 
+import           Control.Concurrent hiding                    (yield)
 import           Control.Concurrent.STM
 import           Control.DeepSeq
 import           Control.Exception
-import           Control.Lens hiding                   ((<.>))
+import           Control.Lens hiding                          ((<.>))
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Primitive
@@ -30,23 +32,71 @@ import           Data.Primitive.MutVar
 import           Data.Time
 import           Data.Traversable
 import           Data.Tuple
-import           GHC.Generics                          (Generic)
+import           GHC.Generics                                 (Generic)
 import           GHC.TypeLits
-import           Numeric.Backprop
-import           Numeric.LinearAlgebra.Static.Backprop
+import           Numeric.Backprop hiding                      (auto)
+import           Numeric.LinearAlgebra.Static.Backprop hiding ((<>))
 import           Numeric.OneLiner
-import           Numeric.Opto hiding                   ((<.>))
+import           Numeric.Opto hiding                          ((<.>))
 import           Numeric.Opto.Backprop
 import           Options.Applicative
-import           System.Environment
-import           System.FilePath hiding                ((<.>))
+import           System.FilePath hiding                       ((<.>))
 import           Text.Printf
-import qualified Data.Conduit.Combinators              as C
-import qualified Data.Text                             as T
-import qualified Data.Vector.Generic                   as VG
-import qualified Numeric.LinearAlgebra                 as HM
-import qualified Numeric.LinearAlgebra.Static          as H
-import qualified System.Random.MWC                     as MWC
+import qualified Data.Conduit.Combinators                     as C
+import qualified Data.Text                                    as T
+import qualified Data.Vector.Generic                          as VG
+import qualified Numeric.LinearAlgebra                        as HM
+import qualified Numeric.LinearAlgebra.Static                 as H
+import qualified System.Random.MWC                            as MWC
+
+data OMode = OSingle
+           | OParallel Bool Int
+
+data Opts = Opts
+    { oDataDir :: FilePath
+    , oReport  :: Int
+    , oBatch   :: Int
+    , oMode    :: OMode
+    }
+
+parseOMode :: Parser OMode
+parseOMode = subparser
+    ( command "single" (info (pure OSingle) (progDesc "Single-threaded"))
+   <> command "parallel" (info (uncurry OParallel <$> parseParallel) (progDesc "Parallel"))
+    )
+  where
+    parseParallel :: Parser (Bool, Int)
+    parseParallel = (,)
+        <$> switch (long "chunked" <> help "Chunked mode")
+        <*> option auto
+                ( long "split"
+               <> short 's'
+               <> help "Number of items per thread"
+               <> metavar "INT"
+               <> showDefault
+               <> value 750
+                )
+
+parseOpts :: Parser Opts
+parseOpts = Opts
+    <$> strArgument ( help "Data directory" <> metavar "DIR" )
+    <*> option auto
+          ( long "report"
+         <> short 'r'
+         <> help "Report frequency (in batches)"
+         <> metavar "INT"
+         <> showDefault
+         <> value 2500
+          )
+    <*> option auto
+          ( long "batch"
+         <> short 'b'
+         <> help "Batching amount"
+         <> metavar "INT"
+         <> showDefault
+         <> value 1
+          )
+    <*> (parseOMode <|> pure OSingle)
 
 data Net = N { _weights1 :: !(L 250 784)
              , _bias1    :: !(R 250)
@@ -105,21 +155,28 @@ netErr x targ n = crossEntropy targ (runNet n x)
 
 main :: IO ()
 main = MWC.withSystemRandom $ \g -> do
-    datadir:mode <- getArgs
-    Just train <- loadMNIST (datadir </> "train-images-idx3-ubyte")
-                            (datadir </> "train-labels-idx1-ubyte")
-    Just test  <- loadMNIST (datadir </> "t10k-images-idx3-ubyte")
-                            (datadir </> "t10k-labels-idx1-ubyte")
+    Opts{..} <- execParser $ info (parseOpts <**> helper)
+        ( fullDesc
+       <> progDesc "Run optimizer samples on MNIST"
+       <> header "opto-neural - opto runner on MNIST data set"
+        )
+
+    Just train <- loadMNIST (oDataDir </> "train-images-idx3-ubyte")
+                            (oDataDir </> "train-labels-idx1-ubyte")
+    Just test  <- loadMNIST (oDataDir </> "t10k-images-idx3-ubyte")
+                            (oDataDir </> "t10k-labels-idx1-ubyte")
     putStrLn "Loaded data."
     net0 <- MWC.uniformR (-0.5, 0.5) g
+    n    <- getNumCapabilities
     sampleQueue <- atomically $ newTBQueue 25000
 
     let o :: PrimMonad m => Opto m (MutVar (PrimState m) Net) (R 784, R 10) Net
         o = adam @_ @(MutVar _ Net) def
                (bpGradSample $ \(x, y) -> netErr (constVar x) (constVar y))
 
-        ro = def { roFreq  = Just 2500 }
-        po = def { poSplit = 750       }
+        ro = def { roBatch = oBatch }
+        -- ro = def { roFreq  = Just 2500 }
+        -- po = def { poSplit = 750       }
 
         report b = do
           yield $ printf "(Batch %d)\n" (b :: Int)
@@ -138,26 +195,26 @@ main = MWC.withSystemRandom $ \g -> do
               yield $ printf "Training error:   %.2f%%\n" ((1 - trainScore) * 100)
               yield $ printf "Validation error: %.2f%%\n" ((1 - testScore ) * 100)
 
-    case mode of
-      "parallel":_ -> runConduit $
-            optoConduitPar ro po net0 o
-                ( forM_ [0..] (\e -> liftIO (printf "[Epoch %d]\n" (e :: Int))
-                                  >> C.yieldMany train .| shuffling g
-                              )
-                  .| C.iterM (atomically . writeTBQueue sampleQueue)
-                )
-         .| mapM_ report [0..]
-         .| C.map T.pack
-         .| C.encodeUtf8
-         .| C.stdout
+        source = forM_ [0..] (\e -> liftIO (printf "[Epoch %d]\n" (e :: Int))
+                                 >> C.yieldMany train .| shuffling g
+                             )
+              .| C.iterM (atomically . writeTBQueue sampleQueue)
 
-      _ -> runConduit $
-            forM_ [0..] (\e -> liftIO (printf "[Epoch %d]\n" (e :: Int))
-                            >> C.yieldMany train .| shuffling g
-                        )
-         .| C.iterM (atomically . writeTBQueue sampleQueue)
-         .| optoConduit ro net0 o
-         .| forever (C.drop 2499 *> (mapM_ yield =<< await))
+        optimizer = case oMode of
+          OSingle -> source
+                  .| optoConduit ro net0 o
+                  .| forever (C.drop (oReport - 1) *> (mapM_ yield =<< await))
+          OParallel c s ->
+            let po      = def { poSplit = s }
+                skipAmt = max 0 $ (oReport `mod` (n * s)) - 1
+                source'
+                  | c         = optoConduitParChunk ro po net0 o source
+                  | otherwise = optoConduitPar      ro po net0 o source
+            in  source'
+                  .| forever (C.drop skipAmt *> (mapM_ yield =<< await))
+
+    runConduit $
+            optimizer
          .| mapM_ report [0..]
          .| C.map T.pack
          .| C.encodeUtf8
